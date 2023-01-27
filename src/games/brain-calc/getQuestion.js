11101e3d14dfb8bf4c80f1b1e361b3169da85425
Andrey Koliukh <kolldronzr@yandex.ru>
2023-01-27 15:56:48 +0600

    update src/games/brain-calc/getQuestion.js

--- src/games/brain-calc/getQuestion.js
+++ src/games/brain-calc/getQuestion.js
@@ -1,15 +1,15 @@
 function getQuestion(max = 25) {
     const getRandomOperator = Math.floor(Math.random() * 3);
-    let result;
+    let result = '0 + 0';
     switch (getRandomOperator) {
         case 0:
             result = `${Math.floor(Math.random() * max)} + ${Math.floor(Math.random() * max)}`;
             break;
         case 1:
-            result = `${Math.floor(Math.random() * max)} - ${Math.floor(Math.random() * max)}`
+            result = `${Math.floor(Math.random() * max)} - ${Math.floor(Math.random() * max)}`;
             break;
         case 2:
-            result = `${Math.floor(Math.random() * max)} * ${Math.floor(Math.random() * max)}`
+            result = `${Math.floor(Math.random() * max)} * ${Math.floor(Math.random() * max)}`;
             break;
     };
 
