f1d39f0abad9a2971d24e341031418a9a7a22aec
Andrey Koliukh <kolldronzr@yandex.ru>
2023-02-01 17:00:02 +0600

    fix src/games/brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -7,7 +7,7 @@ const generateRound = () => {
   arrayNumbers.push(getRandom(0, 50));
 
   const progressionLength = getRandom(5, 12);
-  const hiddenElementIndex = getRandom(0, progressionLength - 1);
+  const hiddenElementIndex = getRandom(1, progressionLength - 1);
   let hiddenElement = 0;
 
   for (let i = 1; i <= progressionLength; i += 1) {
